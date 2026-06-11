4ccd0e84749735aca068bdc2a993de31262c5584
Almhseri33 <mmhsyry46@gmail.com>
2026-06-11 22:58:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -695,4 +695,6 @@ if __name__ == '__main__':
     print("\n" + "="*60 + "\n")
     
     # تشغيل السيرفر
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
